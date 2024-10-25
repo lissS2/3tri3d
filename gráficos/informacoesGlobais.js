0b049ac57f637e8bbf4a0eaa04d3444cad0331a5
Luisa Zanardini <106076128+lissS2@users.noreply.github.com>
2024-10-25 08:45:24 -0300

    .

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -1,4 +1,24 @@
 const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url);
-kmkm
+const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais() {
+   const res = await fetch(url)
+   const dados = await res.json()
+   console.log(dados)
+   const paragrafo = document.createElement('p')
+   paragrafo.classList.add('graficos-container__texto')
+   paragrafo.innerHTML = `Você sabia que o mundo /
+    tem <span>${dados.total_pessoas_mundo}</span> /
+     de pessoas e que aproximadamente /
+     <span>${dados.total_pessoas_conectadas}</span> estão /
+     conectadas em alguma rede social e passam /
+     em média <span>${dados.tempo_medio}</span> /
+     horas conectadas.`
+     console.log(paragrafo)
+
+     const container = document.getElementById('graficos-container')
+     container.appendChild(paragrafo)
+}
+
+visualizarInformacoesGlobais()
